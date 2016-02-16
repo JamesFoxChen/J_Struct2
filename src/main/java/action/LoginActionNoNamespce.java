8be3dd1a28fada53ae21a008f6248e4139ca05a6
JamesFoxChen <25717594@qq.com>
2016-02-16 18:09:40 +0800

    完善ajax请求

--- src/main/java/action/LoginActionNoNamespce.java
+++ src/main/java/action/LoginActionNoNamespce.java
@@ -1,38 +1,38 @@
 package action;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.interceptor.validation.SkipValidation;
 
+import freemarker.template.SimpleDate;
+
 @ParentPackage("json-default")
+@Namespace("/login1")
 public class LoginActionNoNamespce extends SuperActionSupport {
 	private static final long serialVersionUID = 1L;
 
-	private String userName;
-	private String password;
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+	/*
+	 * private String userName; private String password;
+	 * 
+	 * public String getPassword() { return password; }
+	 * 
+	 * public void setPassword(String password) { this.password = password; }
+	 * 
+	 * public String getUserName() { return userName; }
+	 * 
+	 * public void setUserName(String userName) { this.userName = userName; }
+	 */
 
 	private Map<String, String> dataMap;
 
+	//会把本类所有getter方法序列化成字符串返回给jsp页面
 	public Map<String, String> getDataMap() {
 		return dataMap;
 	}
@@ -49,8 +49,9 @@ public class LoginActionNoNamespce extends SuperActionSupport {
 		// 参数名称必须和jsp的空间名称一一对应
 		String userName = request.getParameter("userName");
 		String password = request.getParameter("password");
-		dataMap.put("userName", "Hi:" + userName);
-		dataMap.put("password", password);
+		dataMap.put("isSuccess", "true");
+		SimpleDateFormat now = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒");
+		dataMap.put("sayHi", "Hi:" + userName + "  当前时间为：" + now.format(new Date()) + password);
 
 		return SUCCESS;
 
--- src/main/webapp/WEB-INF/content/LoginInfo/login.jsp
+++ src/main/webapp/WEB-INF/content/LoginInfo/login.jsp
@@ -2,8 +2,9 @@
 	pageEncoding="UTF-8"%>
 <%@ taglib uri="/struts-tags" prefix="s"%>
 <%
-String path = request.getContextPath();
-String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+	String path = request.getContextPath();
+	String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort()
+			+ path + "/";
 %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -16,27 +17,31 @@ String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.
 </head>
 <body>
 	<form action="loginValidate" method="post">
-		用户名： <input type="text" name="userName"> 密码： <input
-			type="text" name="password"> <input type="submit" value="登录">
+		用户名： <input type="text" id="userName"> 密码： <input type="text"
+			id="password"> <input type="submit" value="登录">
 		<s:fielderror fieldName="userName" />
 	</form>
-
-
 	<input type="button" id="btnAjax" value="Ajax调用" />
-
 	<script type="text/javascript">
 		$(function() {
 			$("#btnAjax").click(function() {
+				var userName = $("#userName").val().trim();
+				var password = $("#password").val().trim();
+				if (userName == '') {
+					alert("用户名不能为空");
+					return false;
+				}
+				
 				$.ajax({
 					type : "post",
-					url : 'ajaxExecute',
+					url : '/login1/ajaxExecute',
 					data : {//设置数据源
-						userName : 'James',
-						password : '28'
+						userName : userName,
+						password : "我是密码"
 					},
 					dataType : "json",//设置需要返回的数据类型
 					success : function(d) {
-                        alert(d.userName);
+						alert(d.sayHi);
 					},
 					error : function(d) {
 						alert(d.responseText);
